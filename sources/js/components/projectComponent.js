caf40d7f4cac8c3c410992b60b9f800908748864
Felix <felavalos@gmail.com>
2024-08-26 19:40:43 -0600

    Create card function
    Create and get the card of the project
    Save the "card" access to the dom in the class property "card"
    Create a const above the class "doc" short of document;

--- sources/js/components/projectComponent.js
+++ sources/js/components/projectComponent.js
@@ -1,3 +1,5 @@
+const doc = document;
+
 class Project extends HTMLElement {
   id;
   card;
@@ -7,7 +9,17 @@ class Project extends HTMLElement {
     super();
 
     this.id = id;
+    this.card = this.createCard;
   }
+
+  /* ---------------------Card functions--------------------- */
+  createCard = () => {
+    const projects = doc.getElementById("projects__container");
+
+    projects.appendChild(doc.createElement('div'));
+
+    return projects.lastElementChild;
+  };
 }
 
 export default Project;
