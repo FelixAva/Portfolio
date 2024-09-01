ed697216c03b82716821b2282dd4f65dae79e69c
Felix <felavalos@gmail.com>
2024-09-01 14:56:43 -0600

    Create changeCurrentThemeValue function
    Create a function to change the current theme value
    Create a conditional to check the theme number is in the limits
    	If it is not, return the current value to 0
    If the value is in the limits, increase it by one

--- sources/js/theme-manager/theme-manager.js
+++ sources/js/theme-manager/theme-manager.js
@@ -6,4 +6,10 @@ export class ThemeManager {
   getCurrentThemeValue = () => {
     this.currentTheme = Number( localStorage.getItem("theme") ); // If the theme value is undefined, the "Number()" will trasnform it into a 0
   };
+
+  changeCurrentThemeValue = () => {
+    if (this.theme >= themes.length - 1) return this.theme = 0;
+
+    this.theme++;
+  };
 }
