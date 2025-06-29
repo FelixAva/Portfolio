9941d606114b125108889bbb93e59c9552ab7964
Felix Avalos <felavalosg@gmail.com>
2025-06-30 05:03:52 +0900

    Navigation HTML skeleton

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,6 +3,16 @@ const NavBar = () => {
     <div>
       <h2>Software Engineer</h2>
       <img src="/src/assets/react.svg" alt="" />
+
+      <div>
+        <a href="">Summary</a>
+        <a href="">Projects</a>
+        <a href="">Archievements & Certifications</a>
+        <a href="">Stack</a>
+
+        <button>Curriculum + Icon</button> {/* Replace "+ Icon" with an Icon tag */}
+        <button hidden>Theme</button>
+      </div>
     </div>
   );
 }
