61a630fb80d93f8bfcfd5cd7203885ecdaaa29d1
Felix Avalos <felavalosg@gmail.com>
2025-06-30 04:56:07 +0900

    Rendering the navigation bar

---
+++ src/LandingPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Component's imports
+import { NavBar } from './components';
+
+const LandingPage = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default LandingPage;
