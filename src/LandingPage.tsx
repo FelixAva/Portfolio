6e4a265e04a0dee230c84f7c1ceb6d0f0dadc806
Felix Avalos <felavalosg@gmail.com>
2025-06-30 06:50:42 +0900

    NavBar styling

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -7,6 +7,9 @@ const LandingPage = () => {
   return (
     <div>
       <NavBar />
+      <main className='pt-[96px]'>
+        <section className='min-h-[calc(100vh-96px)] bg-[#EFE9E3]'></section>
+      </main>
     </div>
   );
 };
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,21 @@
 const NavBar = () => {
   return (
-    <div className="flex justify-between">
-      <div className="flex">
-        <img src="/src/assets/react.svg" alt="" />
+    <div className="fixed top-0 left-0 w-full h-24 bg-amber-100 z-50  flex pb-2 pt-2 pl-10 pr-10 justify-between items-center">
+      <div className="flex gap-5 items-center">
+        <img src="/src/assets/logo_crop.png" alt="" className="h-15 rounded-full"/>
         <h2>Software Engineer</h2>
       </div>
 
-      <div className="">
-        <a href="">Summary</a>
-        <a href="">Projects</a>
-        <a href="">Archievements & Certifications</a>
-        <a href="">Stack</a>
+      <div className="flex gap-10">
+        <nav className="flex gap-5">
+          <a href="">Summary</a>
+          <b>|</b>
+          <a href="">Projects</a>
+          <b>|</b>
+          <a href="">Archievements & Certifications</a>
+          <b>|</b>
+          <a href="">Stack</a>
+        </nav>
 
         <button>Curriculum + Icon</button> {/* Replace "+ Icon" with an Icon tag */}
         <button hidden>Theme</button>
