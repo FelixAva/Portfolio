cb2795d53f29fc48a50c113949b1125410d2dba9
Felix Avalos <felavalosg@gmail.com>
2025-07-01 12:57:44 +0900

    Summary styling, scrolling and projects section

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,24 +1,26 @@
 const NavBar = () => {
   return (
-    <div className="fixed top-0 left-0 w-full h-24 bg-amber-100 z-50  flex pb-2 pt-2 pl-10 pr-10 justify-between items-center">
+    <div className="fixed top-0 left-0 w-full h-24 z-50 bg-white flex pb-2 pt-2 pl-10 pr-10 justify-between items-center">
       <div className="flex gap-5 items-center">
         <img src="/src/assets/logo_crop.png" alt="" className="h-15 rounded-full"/>
-        <h2>Software Engineer</h2>
+        <h2 className="text-xl text-[#2e2e2e]">Software Engineer</h2>
       </div>
 
       <div className="flex gap-10">
-        <nav className="flex gap-5">
-          <a href="">Summary</a>
+        <nav className='flex gap-5 text-2xl'>
+          <a href="#summary" className='hover:text-[#8d8d8d]' >Summary</a>
           <b>|</b>
-          <a href="">Projects</a>
+          <a href="#projects">Projects</a>
           <b>|</b>
-          <a href="">Archievements & Certifications</a>
+          <a href="#a">Archievements & Certifications</a>
           <b>|</b>
           <a href="">Stack</a>
         </nav>
 
-        <button>Curriculum + Icon</button> {/* Replace "+ Icon" with an Icon tag */}
-        <button hidden>Theme</button>
+        <div className="text-xl">
+          <button>Curriculum + Icon</button> {/* Replace "+ Icon" with an Icon tag */}
+          <button hidden>Theme</button>
+        </div>
       </div>
     </div>
   );
