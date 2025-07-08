7617ac12cdb6083fd5dd02b6c5906c48168c22bc
Felix Avalos <felavalosg@gmail.com>
2025-07-09 06:08:25 +0900

    Card html structure

--- src/components/project/ProjectCard.tsx
+++ src/components/project/ProjectCard.tsx
@@ -9,7 +9,21 @@ const ProjectCard = ({
 }: ProjectDetails) => {
   return (
     <div id={title.toLowerCase() + id}>
-      {title + id}
+      <div>
+        <img src="https://placehold.co/250X250.png" alt={title + "'s image representation"} className="rounded-2xl" />
+
+        <h3>{ title }</h3>
+        <div>
+          <button>Github</button>
+          <button>Details</button>
+        </div>
+      </div>
+
+      <div>
+        Technologies map
+      </div>
+
+      <p>{ description }</p>
     </div>
   )
 };
