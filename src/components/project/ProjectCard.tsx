8f46ee8fd6350708760196516588e0fc9e2ad7ab
Felix Avalos <felavalosg@gmail.com>
2025-07-09 06:48:06 +0900

    Initial styles

--- src/components/project/ProjectCard.tsx
+++ src/components/project/ProjectCard.tsx
@@ -8,22 +8,24 @@ const ProjectCard = ({
   image
 }: ProjectDetails) => {
   return (
-    <div id={title.toLowerCase() + id}>
-      <div>
+    <div id={title.toLowerCase() + id} className="w-xl justify-between">
+      <div className="flex">
         <img src="https://placehold.co/250X250.png" alt={title + "'s image representation"} className="rounded-2xl" />
 
-        <h3>{ title }</h3>
-        <div>
-          <button>Github</button>
-          <button>Details</button>
+        <div className="w-full flex flex-col text-center justify-between py-4">
+          <h3>{ title }</h3>
+          <div className="gap-2.5 flex flex-col">
+            <button>Details + icon</button>
+            <button>Github + icon</button>
+          </div>
         </div>
       </div>
 
-      <div>
+      <div className="py-2.5">
         Technologies map
       </div>
 
-      <p>{ description }</p>
+      <p className="w-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus inventore minus excepturi facere optio quasi  necessitatibus modi quis? Nam, ducimus! Laborum ab, pariatur exercitationem sed sint aliquam reiciendis!</p>
     </div>
   )
 };
