4aa2adacf320ad497a6c5a252230636f6f298bfe
Felix Avalos <felavalosg@gmail.com>
2025-07-10 01:36:24 +0900

    Button UI component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,9 @@
 // Layout's imports
 import NavBar from './layout/NavBar';
 
+// UI's imports
+import Button from './ui/Button';
+
 // Section's imports
 import Summary from './sections/Summary';
 import Projects from './sections/Projects';
@@ -11,6 +14,9 @@ import ProjectCard from './project/ProjectCard';
 export {
   // Layout's exports
   NavBar,
+
+  // UI's exports
+  Button,
 
   // Sections's exports
   Summary,
