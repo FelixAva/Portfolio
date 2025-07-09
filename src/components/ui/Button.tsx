156f9bf05eed27b7e5977772f572b45210da5418
Felix Avalos <felavalosg@gmail.com>
2025-07-10 03:44:19 +0900

    pointer

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const Button = ({ title, hasIcon, icon="heart" }: Props) => {
   return (
-    <button className="flex justify-center items-center gap-3.5 border-black border-2 text-xl rounded-lg py-3 px-4 cursor-pointer">
+    <button className="flex justify-center items-center gap-3.5 border-black border-2 text-xl rounded-lg py-3 px-4">
       { title }{ hasIcon && <Icon icon={icon} />}
     </button>
   );
